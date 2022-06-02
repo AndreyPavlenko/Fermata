fe310f37d122035c0118b72277f72db1844973a0
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2022-06-02 23:32:19 +0300

    Libraries update

--- build.sh
+++ build.sh
@@ -7,6 +7,23 @@ DEST_DIR="$DIR/dist"
 mkdir -p "$DEST_DIR"
 export NO_GS=true
 
+if [ -z "$ANDROID_SDK_ROOT" ]; then
+    if [ -f "$DIR/local.properties" ]; then
+        ANDROID_SDK_ROOT="$(grep sdk.dir= local.properties | cut -d = -f2)"
+    fi
+fi
+
+if [ -z "$ANDROID_SDK_ROOT" ]; then
+    echo 'ANDROID_SDK_ROOT environment variable is not set'
+    exit 1
+else
+    echo "ANDROID_SDK_ROOT=$ANDROID_SDK_ROOT"
+fi
+
+CMAKE_PATH="$(find $ANDROID_SDK_ROOT/cmake/* -maxdepth 1 -type d -name bin | sort -V | tail -1)"
+echo "CMAKE_PATH=$CMAKE_PATH"
+export PATH=$CMAKE_PATH:$PATH
+
 build_apk() {
     local sfx='arm64'
     local abi='arm64-v8a'
